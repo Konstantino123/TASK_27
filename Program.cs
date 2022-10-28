90ca040c4beb05a27e9af52972261f08598e4190
Konstantin <kt79@bk.ru>
2022-10-28 09:52:57 +0300

    new

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int s = 0;
+while (N > 0)
+{
+    s = s + N % 10;
+    N  /= 10;
+}
+Console.WriteLine(s);
